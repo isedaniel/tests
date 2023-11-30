66b4df1439375165fb6265249440f94f28890188
Daniel <ise.daniel@gmail.com>
2023-11-29 21:01:59 -0300

    clang-format used for the first time

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 
-int main()
-{
-	std::cout << "Enter a number: ";
-	int x{};
-	std::cin >> x;
-	std::cout << "You entered: " << x << '\n';
-	int y;
-	std::cout << y << '\n';
-	return 0;
+int main() {
+  std::cout << "Enter a number: ";
+  int x{};
+  std::cin >> x;
+  std::cout << "You entered: " << x << '\n';
+  int y;
+  std::cout << y << '\n';
+  return 0;
 }
-
