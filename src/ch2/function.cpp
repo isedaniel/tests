fe06212fa447fc15abace115699b738d15a25b4f
Daniel <ise.daniel@gmail.com>
2023-12-07 19:09:45 -0300

    function with parameters

--- src/ch2/function.cpp
+++ src/ch2/function.cpp
@@ -7,9 +7,14 @@ int getInt() {
   return i;
 }
 
+void printDouble(int i) {
+  std::cout << "Double " << i << ": " << i*2 << '\n';
+}
+
 int main() {
   int i { getInt() };
   std::cout << "You entered " << i << '\n';
+  printDouble(i);
   return 0;
 }
 
