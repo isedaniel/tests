d879f7265f5679fe306c2033869e61394ebcaa33
Daniel <ise.daniel@gmail.com>
2023-12-07 19:17:49 -0300

    polymorphism tests

--- src/ch2/function.cpp
+++ src/ch2/function.cpp
@@ -8,13 +8,19 @@ int getInt() {
 }
 
 void printDouble(int i) {
-  std::cout << "Double " << i << ": " << i*2 << '\n';
+  std::cout << i << " doubled is: " << i*2 << '\n';
+}
+
+void printDouble(int i, int x) {
+  std::cout << "This is doubled doble" << '\n';
+  printDouble(i);
+  printDouble(x);
 }
 
 int main() {
   int i { getInt() };
   std::cout << "You entered " << i << '\n';
-  printDouble(i);
+  printDouble(i, i+1);
   return 0;
 }
 
