55d981d479b8a1465ae80468e74954d5129210c1
Daniel <ise.daniel@gmail.com>
2023-12-11 18:38:42 -0300

    clang-format

--- src/ch2/function.cpp
+++ src/ch2/function.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 
 int getInt(std::string s) {
-  std::string  output { "Enter a number(" + s + "): " };
+  std::string output{"Enter a number(" + s + "): "};
   std::cout << output;
-  int i {};
+  int i{};
   std::cin >> i;
   return i;
 }
 
-void printDouble(int i) {
-  std::cout << i << " doubled is: " << i*2 << '\n';
-}
+void printDouble(int i) { std::cout << i << " doubled is: " << i * 2 << '\n'; }
 
 void printDouble(int x, int y) {
   std::cout << "This is doubled doble" << '\n';
@@ -22,4 +20,3 @@ int main() {
   printDouble(getInt("x"), getInt("y"));
   return 0;
 }
-
