cc9c66e5ffa3d44f98b3724d0346df92e1b34030
Daniel Ise <ise.daniel@gmail.com>
2024-01-03 20:15:20 -0300

    some float experiments, first simple terminal program

---
+++ src/ch4/floats/floats.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <iostream>
+
+int spend(int balance) {
+  if (balance >= 25)
+    return balance - 25;
+  else {
+    std::cout << "Insufficient funds!\n";
+    return balance;
+  }
+}
+
+int main() {
+  int exit { 0 };
+  int balance { 100 };
+
+  while (!exit) {
+    std::string cmd{};
+    std::cout << "cmd: ";
+    std::cin >> cmd;
+
+    if (cmd == "exit")
+      exit = 1;
+    else if (cmd == "balance")
+      std::cout << balance / 100.0 << '\n';
+    else if (cmd == "spend")
+      balance = spend(balance);
+    else 
+      std::cout << "Command not found\n";
+  }
+
+  return 0;
+}
