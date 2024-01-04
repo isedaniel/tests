f584dcbb11a36d0ec80c01df8758899bffe5330f
Daniel Ise <ise.daniel@gmail.com>
2024-01-04 07:28:02 -0300

    clf

--- src/ch4/floats/inf.cpp
+++ src/ch4/floats/inf.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 int main() {
-  double zero {0.0};
-  double posinf { 5.0 / zero };
+  double zero{0.0};
+  double posinf{5.0 / zero};
   std::cout << posinf << '\n';
-  double neginf { -5.0 / zero };
+  double neginf{-5.0 / zero};
   std::cout << neginf << '\n';
-  double nan { zero / zero };
+  double nan{zero / zero};
   std::cout << nan << '\n';
   return 0;
 }
--- src/ch4/floats/precision.cpp
+++ src/ch4/floats/precision.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main() {
-  float f { 123456789.0f };
+  float f{123456789.0f};
   std::cout << std::setprecision(9);
   std::cout << f << '\n';
   return 0;
