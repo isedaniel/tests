782259ce88bb07aa6afb2fd8e8273090a6b9c49e
Daniel Ise <ise.daniel@gmail.com>
2024-01-04 17:58:16 -0300

    clf

--- src/ch4/prime/prime.cpp
+++ src/ch4/prime/prime.cpp
@@ -1,36 +1,33 @@
 #include <iostream>
 
 bool isPrime(int n) {
-	if (n < 2)
-		return false;
+  if (n < 2) return false;
 
-	for (int i=2; i<n; i++) {
-		if (n % i == 0) {
-			return false;
-		}
-	}
-	return true;
+  for (int i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
-bool isInRange(int n) {
-	return (0 <= n && n <= 9);
-}
+bool isInRange(int n) { return (0 <= n && n <= 9); }
 
 int main() {
-	std::cout << "Enter a number between 0 and 9: ";
-	
-	int n{};
-	std::cin >> n;
+  std::cout << "Enter a number between 0 and 9: ";
 
-	if (!isInRange(n)) {
-		std::cout << "Number has to be between 0 and 9!\n";
-		return 1;
-	}
+  int n{};
+  std::cin >> n;
 
-	if (isPrime(n))
-		std::cout << "Number is prime\n";
-	else
-		 std::cout << "Number is not prime\n";
+  if (!isInRange(n)) {
+    std::cout << "Number has to be between 0 and 9!\n";
+    return 1;
+  }
 
-	return 0;
+  if (isPrime(n))
+    std::cout << "Number is prime\n";
+  else
+    std::cout << "Number is not prime\n";
+
+  return 0;
 }
