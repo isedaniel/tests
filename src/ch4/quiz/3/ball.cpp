e3c82f19ec94c591c5e2d72e4b4f11fda0543abd
Daniel <ise.daniel@gmail.com>
2024-01-05 19:05:30 -0300

    ball implemented

--- src/ch4/quiz/3/ball.cpp
+++ src/ch4/quiz/3/ball.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 
 double getHeight() {
-	std::cout << "Enter the height of the tower in meters: ";
-	double height{};
-	std::cin >> height;
-	return height;
+  std::cout << "Enter the height of the tower in meters: ";
+  double height{};
+  std::cin >> height;
+  return height;
+}
+
+double getNewHeight(double height, int second) {
+  double gravity {9.8};
+
+void getHeightAtSecond(double height, int second) {
+  double gravity{9.8};
+  double distanceFallen{gravity * second * second / 2};
+  if (height > distanceFallen)
+    std::cout << "At " << second
+              << " seconds, the ball is at height: " << height - distanceFallen
+              << " meters.\n";
+  else
+    std::cout << "At " << second << " seconds, the ball is on the ground.\n";
 }
 
 int main() {
-	double height{getHeight()};
-	std::cout << "You entered " << height << " meters.\n";
-	return 0;
+  double height{getHeight()};
+  for (int i = 0; i <= 5; i++) {
+    getHeightAtSecond(height, i);
+  }
+  return 0;
 }
