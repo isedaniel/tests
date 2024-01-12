67fc3ea0c60599e4a9813d903af126753a38a8ee
Daniel <ise.daniel@gmail.com>
2024-01-12 17:30:32 -0300

    constexpr func added

--- src/ch5/constexpr/constexpr.cpp
+++ src/ch5/constexpr/constexpr.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+constexpr int max(int x, int y) {
+  return (x > y ? x : y);
+}
+
 int main() {
-  constexpr int x{true ? 2 : 1};
-  std::cout << x << '\n';
+  constexpr int x {1};
+  constexpr int y{true ? 2 : 1};
+  constexpr int z{max(x, y)};
+  std::cout << z << '\n';
   return 0;
 }
