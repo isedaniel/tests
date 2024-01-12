7cea678fa9d8eb773a0afd4e64fb4cbdfc5e8d50
Daniel <ise.daniel@gmail.com>
2024-01-12 18:36:44 -0300

    consteval test

--- src/ch5/constexpr/constexpr.cpp
+++ src/ch5/constexpr/constexpr.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 
-constexpr int max(int x, int y) {
-  return (x > y ? x : y);
-}
+consteval int max(int x, int y) { return (x > y ? x : y); }
 
 int main() {
-  constexpr int x {1};
+  constexpr int x{1};
   constexpr int y{true ? 2 : 1};
   constexpr int z{max(x, y)};
   std::cout << z << '\n';
