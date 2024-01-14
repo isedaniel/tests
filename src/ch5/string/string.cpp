9e5c749ddbc439fab2273b7a49382f8b0c727a91
Daniel <ise.daniel@gmail.com>
2024-01-14 19:35:20 -0300

    quiz question #1 solved (on my own way)

--- src/ch5/string/string.cpp
+++ src/ch5/string/string.cpp
@@ -10,9 +10,14 @@ int main() {
   std::getline(std::cin >> std::ws, dogName);
   std::cout << "Your name is " << name << " and your dog's name is " << dogName
             << ".\n";
-  long nameLen { std::ssize(name) };
+  long nameLen{std::ssize(name)};
   std::cout << "Name len: " << nameLen << '\n';
-  long dogNameLen { std::ssize(dogName) };
+  long dogNameLen{std::ssize(dogName)};
   std::cout << "Dog's name len: " << dogNameLen << '\n';
+  std::cout << "Enter your age: ";
+  long age;
+  std::cin >> age;
+  std::cout << "Name len + Dog's name len + age: " << nameLen + dogNameLen + age
+            << '\n';
   return 0;
 }
