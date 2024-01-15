a24ea9aa532ed3089557787a32ab2345b1aced78
Daniel <ise.daniel@gmail.com>
2024-01-15 14:05:36 -0300

    getting undefined behavior from returning a std::string_view that points to a function scoped std::string

--- src/ch5/string/undefined.cpp
+++ src/ch5/string/undefined.cpp
@@ -2,11 +2,16 @@
 #include <string>
 #include <string_view>
 
-int main() {
-  std::string s{"Hello"};
-  std::string_view sv{s};
+std::string_view getBoolName(bool b) {
+  std::string f { "false" };
+  std::string t { "true" };
+  
+  if (b)
+    return t;
+  return f;
+}
 
-  s = "Good bye !";
-  std::cout << sv << '\n';
+int main() {
+  std::cout << getBoolName(true) << ' ' << getBoolName(false) << '\n';
   return 0;
 }
