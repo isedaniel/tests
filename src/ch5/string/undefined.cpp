2c65252efd370659b9f65352c213d329e71c16cc
Daniel <ise.daniel@gmail.com>
2024-01-15 12:59:19 -0300

    undefined behavior resulting from returning a std::string from a function to initialize a std::string_view

--- src/ch5/string/undefined.cpp
+++ src/ch5/string/undefined.cpp
@@ -2,14 +2,13 @@
 #include <string>
 #include <string_view>
 
+std::string getName() {
+  std::string s{"Danilo"};
+  return s;
+}
+
 int main() {
-std::string_view sv{};
-  {
-    std::string s{ "nested string!\n"};
-    std::cout << "Hello, from nested block!\n";
-    std::cout << s;
-    sv = s;
-  }
-  std::cout << sv;
+  std::string_view sv{getName()};
+  std::cout << sv << '\n';
   return 0;
 }
