14b29f39b9ea975c2df7c5cc1cbd278bcf9d4f08
Daniel <ise.daniel@gmail.com>
2024-01-18 19:32:57 -0300

    the same but using a double: behavior is different

--- src/ch6/arithmetic/operators.cpp
+++ src/ch6/arithmetic/operators.cpp
@@ -3,7 +3,7 @@
 int main() {
   constexpr int apples {12};
   std::cout << "You have " << apples << " apples. Enter how many people to divide them between: ";
-  int people{};
+  double people{};
   std::cin >> people;
   std::cout << "Each person gets " << apples / people << " apples.\n";
   return 0;
