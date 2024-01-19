c34f35540b5085f4d9bfdc6668c6ed5423b82756
Daniel <ise.daniel@gmail.com>
2024-01-19 19:23:02 -0300

    isEven final version

--- src/ch6/isEven/even.cpp
+++ src/ch6/isEven/even.cpp
@@ -3,10 +3,13 @@
 constexpr bool isEven(int n) { return n % 2 == 0; }
 
 int main() {
-  for (int i = -5; i < 5; i++) {
-    if (isEven(i))
-      std::cout << i << " is even\n";
-    else
-      std::cout << i << " is odd\n";
-  }
+  std::cout << "Enter an integer: ";
+  int n{};
+  std::cin >> n;
+
+  if (isEven(n))
+    std::cout << n << " is even\n";
+  else
+    std::cout << n << " is odd\n";
+  return 0;
 }
