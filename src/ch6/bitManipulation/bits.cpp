c9fdba8bd62b20df67edf7b99a6f5b066f87c182
Daniel <ise.daniel@gmail.com>
2024-01-26 20:00:46 -0300

    changing bits with bitset methods set(), flip() and reset()

--- src/ch6/bitManipulation/bits.cpp
+++ src/ch6/bitManipulation/bits.cpp
@@ -5,5 +5,14 @@ int main() {
 
 	std::bitset<8> bits { 0b0000'0101 };
 	std::cout << "All the bits: " << bits << '\n';
+	std::cout << "Setting bit in position 3 to 1: ";
+	bits.set(3);
+	std::cout << bits << '\n';
+	std::cout << "Flipping bit in position 4, from 0 to 1: ";
+	bits.flip(4);
+	std::cout << bits << '\n';
+	std::cout << "Resetting bit in pos 4 to 0: ";
+	bits.reset(4);
+	std::cout << bits << '\n';
 	return 0;
 }
