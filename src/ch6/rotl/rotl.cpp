8f7f58ddade2473a3d02e76a0e7b907ebb21c8b7
Daniel <ise.daniel@gmail.com>
2024-01-30 10:58:35 -0300

    quiz solved using only bitwise operators, so next quiz is also solved

--- src/ch6/rotl/rotl.cpp
+++ src/ch6/rotl/rotl.cpp
@@ -2,16 +2,23 @@
 #include <iostream>
 
 std::bitset<4> rotl(std::bitset<4> bits) {
-	return 0b0010;
+  const std::bitset<4> left{0b1000};
+
+  const bool checkLeft{(left & bits).any()};
+
+  bits <<= 1;
+
+  if (checkLeft) {
+    const std::bitset<4> right{0b0001};
+    bits = (bits | right);
+  }
+  return bits;
 }
 
 int main() {
-	// print boolean values for testing
-	std::cout << std::boolalpha; 	
-
-	std::bitset<4> bits1 { 0b0001 };
-	std::cout << rotl(bits1) << ' ' << (0b0010 == rotl(bits1)) << '\n';
-	std::bitset<4> bits2 { 0b1001 };
-	std::cout << rotl(bits2) << ' ' << (0b0011 == rotl(bits1)) << '\n';
-	return 0;
+  std::bitset<4> bits1{0b0001};
+  std::cout << bits1 << ' ' << rotl(bits1) << '\n';
+  std::bitset<4> bits2{0b1001};
+  std::cout << bits2 << ' ' << rotl(bits2) << '\n';
+  return 0;
 }
