1fb931ff36dd1e969a0c0ec9988246ac24c398b1
Daniel <ise.daniel@gmail.com>
2024-02-01 17:50:14 -0300

    using masks to turn on bits

--- src/ch6/bitmasks/bitmasks.cpp
+++ src/ch6/bitmasks/bitmasks.cpp
@@ -4,10 +4,12 @@
 int main() {
   constexpr std::uint8_t mask0{0b0000'0001};
   constexpr std::uint8_t mask1{0b0000'0010};
-  constexpr std::uint8_t mask2{0b0000'0111};
-  constexpr std::uint8_t flags{0b0000'0101};
+  constexpr std::uint8_t mask2{0b0000'0100};
+  std::uint8_t flags{0b0000'0101};
 
   std::cout << "bit 0 is " << ((flags & mask0) ? "on\n" : "off\n");
+  std::cout << "bit 1 is " << ((flags & mask1) ? "on\n" : "off\n");
+  flags |= mask1;
   std::cout << "bit 1 is " << ((flags & mask1) ? "on\n" : "off\n");
   std::cout << "bit 2 is " << ((flags & mask2) ? "on\n" : "off\n");
 
