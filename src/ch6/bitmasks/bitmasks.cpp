5d5c781d80c595a05d03dd7d44098c626f951e67
Daniel <ise.daniel@gmail.com>
2024-02-01 17:55:05 -0300

    using &= ~mask to turn bit off. gcc needs static_cast<std::uint8_t> to do this

--- src/ch6/bitmasks/bitmasks.cpp
+++ src/ch6/bitmasks/bitmasks.cpp
@@ -8,12 +8,14 @@ int main() {
   std::uint8_t flags{0b0000'0101};
 
   std::cout << "bit 0 is " << ((flags & mask0) ? "on\n" : "off\n");
+
   std::cout << "bit 1 is " << ((flags & mask1) ? "on\n" : "off\n");
   flags |= mask1;
   std::cout << "bit 1 is " << ((flags & mask1) ? "on\n" : "off\n");
+
+  std::cout << "bit 2 is " << ((flags & mask2) ? "on\n" : "off\n");
+  flags &= static_cast<std::uint8_t>(~mask2);
   std::cout << "bit 2 is " << ((flags & mask2) ? "on\n" : "off\n");
 
-  std::cout << "(flags & mask1) " << (flags & mask1) << '\n';
-  std::cout << "(flags & mask2) " << (flags & mask2) << '\n';
   return 0;
 }
