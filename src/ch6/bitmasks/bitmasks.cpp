492ab355a057399ddf183997f9afa8553f491687
Daniel <ise.daniel@gmail.com>
2024-02-02 20:24:25 -0300

    problem 2 solved

--- src/ch6/bitmasks/bitmasks.cpp
+++ src/ch6/bitmasks/bitmasks.cpp
@@ -3,15 +3,18 @@
 #include <iostream>
 
 int main() {
-  [[maybe_unused]] constexpr std::uint8_t opt_viewed{ 0x01 };
-  [[maybe_unused]] constexpr std::uint8_t opt_edited{ 0x02 };
-  [[maybe_unused]] constexpr std::uint8_t opt_favorited{ 0x04 };
-  [[maybe_unused]] constexpr std::uint8_t opt_shared{ 0x08 };
-  [[maybe_unused]] constexpr std::uint8_t opt_deleted{ 0x10 };
+  [[maybe_unused]] constexpr std::uint8_t opt_viewed{0x01};
+  [[maybe_unused]] constexpr std::uint8_t opt_edited{0x02};
+  [[maybe_unused]] constexpr std::uint8_t opt_favorited{0x04};
+  [[maybe_unused]] constexpr std::uint8_t opt_shared{0x08};
+  [[maybe_unused]] constexpr std::uint8_t opt_deleted{0x10};
 
-  std::uint8_t myArticleFlags{ opt_favorited | opt_viewed };
+  std::uint8_t myArticleFlags{opt_favorited | opt_viewed};
 
-  std::cout << std::bitset<8>{ myArticleFlags } << '\n';
+  std::cout << std::bitset<8>{myArticleFlags} << '\n';
+
+  std::cout << "article deleted? "
+            << (myArticleFlags & opt_deleted ? "yes\n" : "no\n");
 
   return 0;
 }
