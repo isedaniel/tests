4aa5be0c2ae57393b67dba857d5592ab4dfb83b6
Daniel <ise.daniel@gmail.com>
2024-02-02 20:18:22 -0300

    solving bitmasks quiz, now question #1

--- src/ch6/bitmasks/bitmasks.cpp
+++ src/ch6/bitmasks/bitmasks.cpp
@@ -1,21 +1,17 @@
+#include <bitset>
 #include <cstdint>
 #include <iostream>
 
 int main() {
-  constexpr std::uint8_t mask0{0b0000'0001};
-  constexpr std::uint8_t mask1{0b0000'0010};
-  constexpr std::uint8_t mask2{0b0000'0100};
-  std::uint8_t flags{0b0000'0101};
+  [[maybe_unused]] constexpr std::uint8_t opt_viewed{ 0x01 };
+  [[maybe_unused]] constexpr std::uint8_t opt_edited{ 0x02 };
+  [[maybe_unused]] constexpr std::uint8_t opt_favorited{ 0x04 };
+  [[maybe_unused]] constexpr std::uint8_t opt_shared{ 0x08 };
+  [[maybe_unused]] constexpr std::uint8_t opt_deleted{ 0x10 };
 
-  std::cout << "bit 0 is " << ((flags & mask0) ? "on\n" : "off\n");
+  std::uint8_t myArticleFlags{ opt_favorited };
 
-  std::cout << "bit 1 is " << ((flags & mask1) ? "on\n" : "off\n");
-  flags |= mask1;
-  std::cout << "bit 1 is " << ((flags & mask1) ? "on\n" : "off\n");
-
-  std::cout << "bit 2 is " << ((flags & mask2) ? "on\n" : "off\n");
-  flags &= static_cast<std::uint8_t>(~mask2);
-  std::cout << "bit 2 is " << ((flags & mask2) ? "on\n" : "off\n");
+  std::cout << std::bitset<8>{ myArticleFlags } << '\n';
 
   return 0;
 }
