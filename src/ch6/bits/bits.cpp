0813eee1f655e09ebb865d1ff9ca90bfcc8ece3e
Daniel <ise.daniel@gmail.com>
2024-02-06 19:32:50 -0300

    creating my own bitwise printer

---
+++ src/ch6/bits/bits.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  int b1{};
+  int b2{};
+  int b4{};
+  int b8{};
+  int b16{};
+  int b32{};
+  int b64{};
+  int b128{};
+  std::cout << b128 << b64 << b32 << b16 << ' ' << b8 << b4 << b2 << b1 << '\n';
+  return 0;
+}
