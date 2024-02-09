c97826c4529a85c616b9f9c9136ede6679f559df
Daniel Ise <ise.daniel@gmail.com>
2024-02-09 11:10:41 -0300

    formatting

--- src/ch7/scope/scope.cpp
+++ src/ch7/scope/scope.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
 int main() {
-	int x{ 9 };
-	std::cout << x << '\n';
-	{
-		int x { 1 };
-		std::cout << x << '\n';
-	}
-	std::cout << x << '\n';
-	return 0;
+  int x{9};
+  std::cout << x << '\n';
+  {
+    int x{1};
+    std::cout << x << '\n';
+  }
+  std::cout << x << '\n';
+  return 0;
 }
