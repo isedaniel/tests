74e079ff125bd5cff0009a3d0d90497e77b8ef6d
Daniel Ise <ise.daniel@gmail.com>
2024-02-13 17:51:40 -0300

    testing on usage of global variables

--- src/ch7/global/global.cpp
+++ src/ch7/global/global.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 
 namespace Foo {
-int g_x;
-constexpr double g_y {9.8};
-}
+int g_x{12};
+constexpr double g_y{};
+} // namespace Foo
 
-void function() {
-  std::cout << "Printing from function(): " << Foo::g_x << '\n';
-  std::cout << "Printing Foo::g_y from function(): " << Foo::g_y << '\n';
-}
+
+void function(int x);
 
 int main() {
   std::cout << "Printing from main(): " << Foo::g_x << '\n';
-  function();
+  function(Foo::g_x);
   Foo::g_x = 5;
   std::cout << "Changing and reprinting from main(): " << Foo::g_x << '\n';
-  function();
+  function(Foo::g_x);
+
+  int x {10};
+  {
+    int x {12};
+    std::cout << x << '\n';
+  }
+
   return 0;
 }
+
---
+++ src/ch7/global/local.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void function(int x) {
+  std::cout << x << '\n';
+}
