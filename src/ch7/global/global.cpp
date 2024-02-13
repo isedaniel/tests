8c41d4d2667103668aa1d210349aef68c9eedfee
Daniel Ise <ise.daniel@gmail.com>
2024-02-13 17:00:51 -0300

    constexpr global constant

--- src/ch7/global/global.cpp
+++ src/ch7/global/global.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 
-int g_x{};
+namespace Foo {
+int g_x;
+constexpr double g_y {9.8};
+}
 
 void function() {
-  std::cout << "Printing from function(): " << g_x << '\n';
+  std::cout << "Printing from function(): " << Foo::g_x << '\n';
+  std::cout << "Printing Foo::g_y from function(): " << Foo::g_y << '\n';
 }
 
 int main() {
-  std::cout << "Printing from main(): " << g_x << '\n';
+  std::cout << "Printing from main(): " << Foo::g_x << '\n';
   function();
-  g_x = 5;
-  std::cout << "Changing and reprinting from main(): " << g_x << '\n';
+  Foo::g_x = 5;
+  std::cout << "Changing and reprinting from main(): " << Foo::g_x << '\n';
   function();
   return 0;
 }
