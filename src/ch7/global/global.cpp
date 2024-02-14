fce9a3fa155a0b829ae0ace786f52ea091048929
Daniel Ise <ise.daniel@gmail.com>
2024-02-14 18:07:58 -0300

    forward declared fuctions, defined in different files, have external linkage by default

--- src/ch7/global/global.cpp
+++ src/ch7/global/global.cpp
@@ -2,9 +2,7 @@
 
 namespace Foo {
 int g_x{12};
-constexpr double g_y{};
 } // namespace Foo
-
 
 void function(int x);
 
@@ -14,12 +12,6 @@ int main() {
   Foo::g_x = 5;
   std::cout << "Changing and reprinting from main(): " << Foo::g_x << '\n';
   function(Foo::g_x);
-
-  int x {10};
-  {
-    int x {12};
-    std::cout << x << '\n';
-  }
 
   return 0;
 }
