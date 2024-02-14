ea36b0140aad04f5bf46d9b4fedd19614c3e373b
Daniel Ise <ise.daniel@gmail.com>
2024-02-14 18:36:34 -0300

    but it's better to maintain modularity

--- src/ch7/global/global.cpp
+++ src/ch7/global/global.cpp
@@ -2,14 +2,14 @@
 
 int g_x{12};
 
-void function();
+void function(int x);
 
 int main() {
   std::cout << "Printing from main(): " << g_x << '\n';
-  function();
+  function(g_x);
   g_x = 5;
   std::cout << "Changing and reprinting from main(): " << g_x << '\n';
-  function();
+  function(g_x);
 
   return 0;
 }
--- src/ch7/global/local.cpp
+++ src/ch7/global/local.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-extern int g_x;
-
-void function() {
-  std::cout << "Printing from function(): " << g_x << '\n';
+void function(int x) {
+  std::cout << "Printing from function(): " << x << '\n';
 }
