b654e79138e92e4edcbe2e9b2704e7f07ef53088
Daniel Ise <ise.daniel@gmail.com>
2024-02-14 18:37:20 -0300

    and use namespaces for global variables

--- src/ch7/global/global.cpp
+++ src/ch7/global/global.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 
+namespace constants {
 int g_x{12};
+}
 
 void function(int x);
 
 int main() {
-  std::cout << "Printing from main(): " << g_x << '\n';
-  function(g_x);
-  g_x = 5;
-  std::cout << "Changing and reprinting from main(): " << g_x << '\n';
-  function(g_x);
+  std::cout << "Printing from main(): " << constants::g_x << '\n';
+  function(constants::g_x);
+  constants::g_x = 5;
+  std::cout << "Changing and reprinting from main(): " << constants::g_x << '\n';
+  function(constants::g_x);
 
   return 0;
 }
