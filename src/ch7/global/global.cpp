20154db7540050063f694f8026a81a33177a6165
Daniel Ise <ise.daniel@gmail.com>
2024-02-14 18:12:30 -0300

    making forward declaration of a global variable, to use it in another source file

--- src/ch7/global/global.cpp
+++ src/ch7/global/global.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 
-namespace Foo {
 int g_x{12};
-} // namespace Foo
 
-void function(int x);
+void function();
 
 int main() {
-  std::cout << "Printing from main(): " << Foo::g_x << '\n';
-  function(Foo::g_x);
-  Foo::g_x = 5;
-  std::cout << "Changing and reprinting from main(): " << Foo::g_x << '\n';
-  function(Foo::g_x);
+  std::cout << "Printing from main(): " << g_x << '\n';
+  function();
+  g_x = 5;
+  std::cout << "Changing and reprinting from main(): " << g_x << '\n';
+  function();
 
   return 0;
 }
--- src/ch7/global/local.cpp
+++ src/ch7/global/local.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
-void function(int x) {
-  std::cout << x << '\n';
+extern int g_x;
+
+void function() {
+  std::cout << "Printing from function(): " << g_x << '\n';
 }
