a7c28caedaedb5243b4a205c1c6c68c4dc896a69
Daniel Ise <ise.daniel@gmail.com>
2024-02-21 17:15:12 -0300

    formatting

--- src/ch8/switch/switch.cpp
+++ src/ch8/switch/switch.cpp
@@ -1,35 +1,33 @@
 #include <iostream>
 
-int nPlusOne(int n) {
-	return n + 1;
-}
+int nPlusOne(int n) { return n + 1; }
 
 void printNPlusOne(int n) {
-	std::cout << "N plus one is ";
-	switch (nPlusOne(n)) {
-		case 1:
-		std::cout << "one.\n";
-		return;
-		case 2:
-		std::cout << "two.\n";
-		return;
-		case 3:
-		std::cout << "three.\n";
-		return;
-		default:
-		std::cout << "greater than three.\n";
-		return;
-	}
+  std::cout << "N plus one is ";
+  switch (nPlusOne(n)) {
+  case 1:
+    std::cout << "one.";
+    return;
+  case 2:
+    std::cout << "two.";
+    return;
+  case 3:
+    std::cout << "three.";
+    return;
+  default:
+    std::cout << "greater than three.";
+    return;
+  }
 }
 
 int main() {
-	int n{-1};
-	while (n < 0 || 255 < n) {
-	std::cout << "Enter a number [0-255]: ";
-	std::cin >> n;
-	}
+  int n{-1};
+  while (n < 0 || 255 < n) {
+    std::cout << "Enter a number [0-255]: ";
+    std::cin >> n;
+  }
 
-	std::cout << (0 <= n);
-	printNPlusOne(n);
-	return 0;
+  printNPlusOne(n);
+  std::cout << '\n';
+  return 0;
 }
