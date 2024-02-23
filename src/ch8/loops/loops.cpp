bcf440b638d3d9aeb0aab8c72b11586965e94705
Daniel Ise <ise.daniel@gmail.com>
2024-02-23 13:48:31 -0300

    second act solved

--- src/ch8/loops/loops.cpp
+++ src/ch8/loops/loops.cpp
@@ -13,5 +13,26 @@ int main() {
     std::cout << '\n';
     --outer;
   }
+
+  std::cout << '\n';
+
+  outer = 1;
+  while (outer <= 5) {
+    // spaces = 4, 3, 2, 1
+    int spaces{5 - outer};
+    while (spaces > 0) {
+      std::cout << "  ";
+      --spaces;
+    }
+
+    int num{1};
+    while (num <= outer) {
+      std::cout << num << ' ';
+      ++num;
+    }
+
+    std::cout << '\n';
+    ++outer;
+  }
   return 0;
 }
