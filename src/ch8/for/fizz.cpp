1a6d59911d6bc4aa3a705fc101eee29a4485681a
Daniel Ise <ise.daniel@gmail.com>
2024-02-26 12:39:42 -0300

    question #5 solved

--- src/ch8/for/fizz.cpp
+++ src/ch8/for/fizz.cpp
@@ -2,20 +2,29 @@
 
 void fizzbuzz(int n) {
   for (int i{1}; i <= n; ++i) {
-    if ((i % 3 == 0) && (i % 5 == 0)) {
-      std::cout << "fizzbuzz";
-    } else if (i % 3 == 0) {
+    bool printed{false};
+    if (i % 3 == 0) {
       std::cout << "fizz";
-    } else if (i % 5 == 0) {
+      printed = true;
+    } 
+    if (i % 5 == 0) {
       std::cout << "buzz";
-    } else {
+      printed = true;
+    } 
+    if (i % 7 == 0) {
+      std::cout << "pop";
+      printed = true;
+    }
+
+    if (!printed) {
       std::cout << i;
     }
+
     std::cout << '\n';
   }
 }
 
 int main() {
-  fizzbuzz(15);
+  fizzbuzz(150);
   return 0;
 }
