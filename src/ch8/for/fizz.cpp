51f869830bda4b0507f8e55a8632a60920914c26
Daniel Ise <ise.daniel@gmail.com>
2024-02-26 12:40:01 -0300

    format

--- src/ch8/for/fizz.cpp
+++ src/ch8/for/fizz.cpp
@@ -6,11 +6,11 @@ void fizzbuzz(int n) {
     if (i % 3 == 0) {
       std::cout << "fizz";
       printed = true;
-    } 
+    }
     if (i % 5 == 0) {
       std::cout << "buzz";
       printed = true;
-    } 
+    }
     if (i % 7 == 0) {
       std::cout << "pop";
       printed = true;
