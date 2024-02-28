a633ea579c573d96a8cbe02966a30984ac43608b
Daniel Ise <ise.daniel@gmail.com>
2024-02-28 13:57:19 -0300

    the same but with a dice of 6 sides

--- src/ch8/mersenne/mersenne.cpp
+++ src/ch8/mersenne/mersenne.cpp
@@ -3,9 +3,10 @@
 
 int main() {
   std::mt19937 mt{};
+  std::uniform_int_distribution d6{ 1, 6};
   for (int i{1}; i <= 40; ++i) {
-    std::cout << mt() << '\t';
-    if (i % 5 == 0) {
+    std::cout << d6(mt) << '\t';
+    if (i % 10 == 0) {
       std::cout << '\n';
     }
   }
