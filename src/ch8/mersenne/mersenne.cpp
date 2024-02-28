080648f15dd4be591651a2d6c746ed81e6c3c486
Daniel Ise <ise.daniel@gmail.com>
2024-02-28 14:00:36 -0300

    the same again, but now seeded with system time

--- src/ch8/mersenne/mersenne.cpp
+++ src/ch8/mersenne/mersenne.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <random>
+#include <chrono>
 
 int main() {
-  std::mt19937 mt{};
+  std::mt19937 mt{ static_cast<std::mt19937::result_type>(
+    std::chrono::steady_clock::now().time_since_epoch().count()
+  )};
+
   std::uniform_int_distribution d6{ 1, 6};
   for (int i{1}; i <= 40; ++i) {
     std::cout << d6(mt) << '\t';
