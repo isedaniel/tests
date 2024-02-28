1b0c69c2f88de6714a2c6ebd24c703efca534fa7
Daniel Ise <ise.daniel@gmail.com>
2024-02-28 13:40:33 -0300

    mersenne twister algorithm used from <random>

---
+++ src/ch8/mersenne/mersenne.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <random>
+
+int main() {
+  std::mt19937 mt{};
+  for (int i{1}; i <= 40; ++i) {
+    std::cout << mt() << '\t';
+    if (i % 5 == 0) {
+      std::cout << '\n';
+    }
+  }
+  return 0;
+}
