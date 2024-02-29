597e27db69417cd30773367c299a12e2cb59d108
Daniel Ise <ise.daniel@gmail.com>
2024-02-29 18:39:33 -0300

    random header implemented in main, with a program that provides 50 numbers from a dice of 20 sides

--- src/ch8/random/main.cpp
+++ src/ch8/random/main.cpp
@@ -1,6 +1,13 @@
+#include "random.h"
 #include <iostream>
 
 int main() {
-  std::cout << "Hello, World!\n";
+  std::cout << "Getting random numbers from a d20\n";
+  for (int i{1};i<=100;++i) {
+    std::cout << Random::get(1, 20) << '\t';
+    if (i % 10 == 0) {
+      std::cout << '\n';
+    }
+  }
   return 0;
 }
