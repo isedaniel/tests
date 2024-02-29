9f379f552470d3c7ebd20cdc8e0a9d77d294ffd9
Daniel Ise <ise.daniel@gmail.com>
2024-02-29 14:04:50 -0300

    formatting

--- src/ch8/mersenne/mersenne.cpp
+++ src/ch8/mersenne/mersenne.cpp
@@ -2,9 +2,9 @@
 #include <random>
 
 int main() {
-  std::mt19937 mt{ std::random_device{}() };
+  std::mt19937 mt{std::random_device{}()};
 
-  std::uniform_int_distribution d6{ 1, 6};
+  std::uniform_int_distribution d6{1, 6};
   for (int i{1}; i <= 40; ++i) {
     std::cout << d6(mt) << '\t';
     if (i % 10 == 0) {
