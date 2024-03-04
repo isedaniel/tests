01d26384240192cd301351c587919136efd9f9fd
Daniel Ise <ise.daniel@gmail.com>
2024-03-04 14:04:48 -0300

    format

--- src/ch8/quiz/3/main.cpp
+++ src/ch8/quiz/3/main.cpp
@@ -2,22 +2,21 @@
 #include <iostream>
 
 void game() {
-  std::cout << "Let's play a game. I'm thinking of a number between 1 and 100. You have 7 tries to guess what it is.\n";
+  std::cout << "Let's play a game. I'm thinking of a number between 1 and 100. "
+               "You have 7 tries to guess what it is.\n";
 
-  int number {Random::get(1, 100)};
+  int number{Random::get(1, 100)};
   int guess{0};
 
-  for (int i{1}; i<=7; ++i) {
+  for (int i{1}; i <= 7; ++i) {
     std::cout << "Guess #" << i << ": ";
     std::cin >> guess;
 
     if (guess < number) {
       std::cout << "Your guess is too low.\n";
-    }
-    else if (guess > number) {
+    } else if (guess > number) {
       std::cout << "Your guess is too high.\n";
-    }
-    else {
+    } else {
       std::cout << "Correct! You win!\n";
       return;
     }
@@ -28,7 +27,9 @@ void game() {
 int main() {
   char play{'y'};
   while (play != 'n') {
-    if (play == 'y') { game(); }
+    if (play == 'y') {
+      game();
+    }
 
     std::cout << "Would you like to play again? (y/n) ";
     std::cin >> play;
