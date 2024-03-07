e9b7ac3efc6d0601a0a9a52cc6be62e43e83ccf6
Daniel Ise <ise.daniel@gmail.com>
2024-03-07 18:48:09 -0300

    guess now has to be greater than 0 and less than 100

--- src/ch9/quiz/main.cpp
+++ src/ch9/quiz/main.cpp
@@ -1,25 +1,58 @@
 #include "random.h"
 #include <iostream>
 
-int play(int guess) {
-  std::cout << "Guess #" << guess << ": ";
+int getGuess(int n) {
   int g{};
-  std::cin >> g;
+  while (g < 1 || 100 > g) {
+    std::cout << "Guess #" << n << ": ";
+    std::cin >> g;
+  }
   return g;
+}
+
+bool game(int secretN) {
+  std::cout << "Let's play a game. I'm thinking of a number between 1 and 100. "
+               "You have 7 tries to guess what it is.\n";
+  int guess{};
+  for (int i{1}; i < 8; ++i) {
+    guess = getGuess(i);
+
+    if (guess < secretN) {
+      std::cout << "Your guess is too low.\n";
+    } else if (guess > secretN) {
+      std::cout << "Your guess is too high.\n";
+    } else {
+      return true;
+    }
+  }
+  return false;
 }
 
 int main() {
   char play{'y'};
-  while (play != 'n') {
-    if (play == 'y') {
-      std::cout << "Let's play a game. I'm thinking of a number between 1 and 100. "
-               "You have 7 tries to guess what it is.\n";
-      int random{Random::get(1, 100)};
+
+  while (true) {
+    switch (play) {
+
+    case 'y':
+    case 'Y': {
+      const int secretN{Random::get(1, 100)};
+
+      if (game(secretN)) {
+        std::cout << "Correct! You win!\n";
+      } else {
+        std::cout << "You lose! Number was " << secretN << ".\n";
+      }
+      break;
+    }
+
+    case 'n':
+    case 'N': {
+      return 0;
+    }
     }
 
     std::cout << "Would you like to play again? (y/n) ";
     std::cin >> play;
   }
-
-  return 0;
 }
