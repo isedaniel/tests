c6f7998f561ae8c752b6832b080a5f0e61fb8ea1
Daniel Ise <ise.daniel@gmail.com>
2024-03-11 17:49:19 -0300

    implicit conversion from short to int

--- src/ch10/implicit/implicit.cpp
+++ src/ch10/implicit/implicit.cpp
@@ -3,6 +3,10 @@
 void printInt(int n) { std::cout << n << '\n'; }
 
 int main() {
-  printInt(3.0);
+  char c{};
+  for (short s{1}; s<101; ++s) {
+    c = s;
+    std::cout << s << ' ' << c << '\n';
+  }
   return 0;
 }
