d41aa723465a18d997a41a0b621d27646967b821
Daniel Ise <ise.daniel@gmail.com>
2024-03-12 19:05:33 -0300

    using static_cast<T> instead of c-type casting is more explicit and less prone to compiler errors

--- src/ch10/implicit/implicit.cpp
+++ src/ch10/implicit/implicit.cpp
@@ -3,13 +3,7 @@
 void printInt(int n) { std::cout << n << '\n'; }
 
 int main() {
-  char c{};
-  for (short s{1}; s<101; ++s) {
-    c = s;
-    std::cout << s << ' ' << c << '\n';
-  }
-
-  int i = 3.5;
-  printInt(i);
+  int n {static_cast<long>(3)};
+  printInt(n);
   return 0;
 }
