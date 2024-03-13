adbe2552a721ecca0e8db8979d2be60fcd656067
Daniel Ise <ise.daniel@gmail.com>
2024-03-13 18:24:08 -0300

    first quiz solved

---
+++ src/ch10/quiz/2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+namespace Constants {
+	inline constexpr double pi { 3.14159 };
+}
+
+using Degrees = double;
+using Radians = double;
+
+Radians convertToRadians(Degrees degrees) {
+	return degrees * Constants::pi / 180;
+}
+
+int main() {
+std::cout << "Enter degrees: ";
+Degrees d{};
+std::cin >> d;
+
+Radians r {convertToRadians(d)};
+std::cout << d << " degrees is " << r << " radians.\n";
+return 0;
+}
