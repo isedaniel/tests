70ed654bd91b831a46040d2b0c9f3293983adbbb
Daniel Ise <ise.daniel@gmail.com>
2024-03-14 17:25:10 -0300

    function can not be overloaded just with different return types

--- src/ch11/overload.cpp
+++ src/ch11/overload.cpp
@@ -8,8 +8,13 @@ double add(double x, double y) {
   return x + y;
 }
 
+double returnOne() {
+  return 1.0;
+}
+
 int main(){
   std::cout << add(1, 2) << '\n';
   std::cout << add(1.1, 2.2) << '\n';
+  std::cout << returnOne() << '\n';
   return 0;
 }
