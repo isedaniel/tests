3631d0ffd8d58170880f20dac08cded4046fe354
Daniel Ise <ise.daniel@gmail.com>
2024-03-14 09:39:55 -0300

    function overloading

---
+++ src/ch11/overload.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int add(int x, int y) {
+  return x + y;
+}
+
+double add(double x, double y) {
+  return x + y;
+}
+
+int main(){
+  std::cout << add(1, 2) << '\n';
+  std::cout << add(1.1, 2.2) << '\n';
+  return 0;
+}
