72264c073dc05d590fe873851bd139547d6e1a58
Daniel Ise <ise.daniel@gmail.com>
2024-03-14 17:37:25 -0300

    ambiguous call: long can be converted to int or double, the compiler can't solve which add() function is called

--- src/ch11/overload.cpp
+++ src/ch11/overload.cpp
@@ -8,13 +8,9 @@ double add(double x, double y) {
   return x + y;
 }
 
-double returnOne() {
-  return 1.0;
-}
-
 int main(){
   std::cout << add(1, 2) << '\n';
   std::cout << add(1.1, 2.2) << '\n';
-  std::cout << returnOne() << '\n';
+  std::cout << add(1L, 4L) << '\n';
   return 0;
 }
