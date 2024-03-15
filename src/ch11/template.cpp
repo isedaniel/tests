68929dfc4459767f39df917b329f9eb125ebe5bc
Daniel Ise <ise.daniel@gmail.com>
2024-03-15 11:06:50 -0300

    print and add template added

--- src/ch11/template.cpp
+++ src/ch11/template.cpp
@@ -6,11 +6,17 @@ T max(T x, T y) {
 }
 
 template <typename T>
-void print(T p) {
-  std::cout << p << '\n';
+void print(T p, char end='\n') {
+  std::cout << p << end;
+}
+
+template <typename T>
+T add(T x, T y) {
+  return x + y;
 }
 
 int main() {
-  print(max(1, 2));
+  print(max<int>(1, 2));
+  print(add<double>(1.2, 3.4));
   return 0;
 }
