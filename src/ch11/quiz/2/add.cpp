85c04573e216d62aa5c074ba28e2fd0dd54a7d57
Daniel Ise <ise.daniel@gmail.com>
2024-03-20 19:49:04 -0300

    step 2 done

--- src/ch11/quiz/2/add.cpp
+++ src/ch11/quiz/2/add.cpp
@@ -5,5 +5,6 @@ template <typename T> T add(T x, T y) { return x + y; }
 int main() {
   std::cout << add(2, 3) << '\n';
   std::cout << add(1.2, 3.4) << '\n';
+
   return 0;
 }
--- src/ch11/quiz/2/mult.cpp
+++ src/ch11/quiz/2/mult.cpp
@@ -5,5 +5,6 @@ template <typename T> T mult(T x, int y) { return x * y; }
 int main() {
   std::cout << mult(2, 3) << '\n';
   std::cout << mult(1.2, 3) << '\n';
+
   return 0;
 }
