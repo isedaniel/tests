ecc506fe0da244e8db13a2763008b50f9515f8df
Daniel Ise <ise.daniel@gmail.com>
2024-03-27 13:45:49 -0300

    isSmall function defined as macro to be able to receive types as arguments (normal function can not)

--- src/ch12/isSmall.cpp
+++ src/ch12/isSmall.cpp
@@ -1,1 +1,17 @@
 #include <iostream>
+
+#define isSmall(T) (sizeof(T) <= 2 * sizeof(void*))
+
+struct S {
+  double a;
+  double b;
+  double c;
+};
+
+int main() {
+  std::cout << std::boolalpha;
+  std::cout << isSmall(int) << '\n';
+  std::cout << isSmall(double) << '\n';
+  std::cout << isSmall(S) << '\n';
+  return 0;
+}
