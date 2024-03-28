85ca4506e67daad66f1188684719a23122567078
Daniel Ise <ise.daniel@gmail.com>
2024-03-28 13:19:56 -0300

    declaring and initializing a pointer. then follow its referece with the de-reference operator *

--- src/ch12/pointers.cpp
+++ src/ch12/pointers.cpp
@@ -6,9 +6,8 @@ void print(const auto& a, const char end = '\n') { std::cout << a << end; }
 
 int main() {
   int n{10};
-  print(n);    // prints n value
-  print(&n);   // prints n memory address
-  print(*&n);  // prints the value at memory adress &n
+  int* ptr_n{&n};
+  print(*ptr_n);
 
   return 0;
 }
